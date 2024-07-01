524be1aaad77adf596822d44a291bfe14950fbab
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-01 11:12:34 -0500

    added faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Clear existing data
+Author.destroy_all
+Book.destroy_all
+Genre.destroy_all
+Publisher.destroy_all
+BookGenre.destroy_all
+BookPublisher.destroy_all
+
+# Create Authors
+10.times do
+  Author.create!(name: Faker::Book.author)
+end
+
+# Create Genres
+10.times do
+  Genre.create!(name: Faker::Book.genre)
+end
+
+# Create Publishers
+10.times do
+  Publisher.create!(name: Faker::Book.publisher)
+end
+
+# Create Books and associate them with Authors, Genres, and Publishers
+50.times do
+  book = Book.create!(
+    title: Faker::Book.title,
+    author: Author.all.sample
+  )
+
+  # Associate books with genres
+  genres = Genre.all.sample(rand(1..3))
+  genres.each do |genre|
+    BookGenre.create!(book: book, genre: genre)
+  end
+
+  # Associate books with publishers
+  publishers = Publisher.all.sample(rand(1..2))
+  publishers.each do |publisher|
+    BookPublisher.create!(book: book, publisher: publisher)
+  end
+end
