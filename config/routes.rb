9c77c03a7c4c88fb63a3c74af6299bee58dd36d8
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-01 23:12:58 -0500

    updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+# config/routes.rb
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
+  get 'search', to: 'pages#search'
   resources :authors, only: [:index, :show]
   resources :books, only: [:index, :show]
   resources :genres, only: [:index, :show]
