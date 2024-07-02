99ce02d2c5fcf1310761ed7ab8fb2b892a638c62
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-01 23:17:49 -0500

    upadated controller files to include pagination

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,51 +1,9 @@
-# app/controllers/authors_controller.rb
 class AuthorsController < ApplicationController
-    before_action :set_author, only: [:show, :edit, :update, :destroy]
-  
     def index
       @authors = Author.page(params[:page])
     end
   
     def show
-    end
-  
-    def new
-      @author = Author.new
-    end
-  
-    def edit
-    end
-  
-    def create
-      @author = Author.new(author_params)
-      if @author.save
-        redirect_to @author, notice: 'Author was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def update
-      if @author.update(author_params)
-        redirect_to @author, notice: 'Author was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @author.destroy
-      redirect_to authors_url, notice: 'Author was successfully destroyed.'
-    end
-  
-    private
-  
-    def set_author
       @author = Author.find(params[:id])
     end
-  
-    def author_params
-      params.require(:author).permit(:name)
-    end
   end
-  
