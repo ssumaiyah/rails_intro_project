bbf0aa3087fe4a32923fa01cde1a384de4a127c0
ssumaiyah <ssumaiyah@academic.rrc.ca>
2024-07-01 23:14:15 -0500

    updated pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,17 @@
+# app/controllers/pages_controller.rb
 class PagesController < ApplicationController
     def index
     end
   
     def about
+    end
+  
+    def search
+      @query = params[:query]
+      @results = Author.where('name LIKE ?', "%#{@query}%") +
+                 Book.where('title LIKE ?', "%#{@query}%") +
+                 Genre.where('name LIKE ?', "%#{@query}%") +
+                 Publisher.where('name LIKE ?', "%#{@query}%")
     end
   end
   
